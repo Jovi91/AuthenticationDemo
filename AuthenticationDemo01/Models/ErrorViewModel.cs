41720071b350e8dc1bba5471ceb42011e5799c40
Jovi <jowita.daszkiewicz@gmail.com>
2020-11-10 10:09:36 +0100

    Add project files.

---
+++ AuthenticationDemo01/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AuthenticationDemo01.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
